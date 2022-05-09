1cc6e829b03e1160e1531902484191ba1ce244c1
rafaelgr <rafa@myariadna.com>
2022-05-09 09:16:19 +0200

    Nuevos lectores

---
+++ WM500V5Solution2/WM500V5Console/Input.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WM500V5Console
+{
+    public class Input
+    {
+        public string command;
+        public string value;
+    }
+}
